d60b64036e3a0405f7e14e9895952ecc16aadef4
KadyntsevAN <60112610+KadyntsevAN@users.noreply.github.com>
2023-10-19 22:25:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {¬¬
+    public static void main(String[] args) {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             System.out.println(calc(reader.readLine()));
